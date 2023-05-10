f617f537a07925ad8f993df7073bebb14aac6100
Jorge Sanz <jorge@jorgesanz.net>
2023-05-10 17:49:56 +0200

    Finished using pmtiles and used pico for the form

--- src/main.js
+++ src/main.js
@@ -4,30 +4,53 @@ import * as pmtiles from "pmtiles";
 
 // CSS Styles
 import './style.css';
+import '../node_modules/@picocss/pico/css/pico.min.css';
 
 
 let protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles",protocol.tile);
 
+let source="pmtiles";
+let style="osm-bright";
+
 const main = () => {
   const map = new maplibregl.Map({
     container: 'map', // container ID
     
-    style: '/styles/vector-tiles/sb-shortbread.json', // style URL
+    style: '', // style URL
     center: [-0.387268,39.473305], // starting position [lng, lat]
     zoom: 12, // starting zoom
+    maxBounds: [-4.51,35.39,4.28,43.53],
     hash: true
   });
 
-  const layerList = document.getElementById('menu');
+  const layerList = document.getElementById('styles');
   const inputs = layerList.getElementsByTagName('input');
+
+  const updateStyle = ()=>{
+    map.setStyle(`/styles/${source}/${style}/style.json`);
+    map.redraw();
+
+    console.log(`🗺 source: ${source}, style:${style}`);
+  }
 
   for (const input of inputs) {
     input.onclick = (layer) => {
-      const layerId = layer.target.id;
-      map.setStyle(`/styles/vector-tiles/${layerId}.json`);
+      style = layer.target.id;
+      updateStyle();
     };
   }
+
+  const sourceList = document.getElementById('sources');
+  const sourceInputs = sourceList.getElementsByTagName('input');
+  for (const input of sourceInputs){
+    input.onclick = (inputSource) => {
+      source = inputSource.target.id;
+      updateStyle();
+    };
+  }
+
+  updateStyle();
 }
 
 
